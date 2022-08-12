883944490acee0b5f00282a64137e6811df358fc
tejas-bts <tdadhe@biztransights.com>
2022-08-12 18:00:36 +0530

    Context logs added for token errors

--- Utils/Authentication.ts
+++ Utils/Authentication.ts
@@ -4,9 +4,11 @@ import { verify } from "jsonwebtoken";
 import { AuthObject } from "../Types/Core.type";
 export const validateToken = async (context: Context, token: string): Promise<AuthObject> => {
   const privateKey = process.env["privateKey"];
+  context.log("Private Key", privateKey, token);
   return new Promise((resolve, reject) => {
     verify(token, privateKey, function (err, authObject) {
       if (err) {
+        context.log("TokenError", err);
         reject(new Error("Invalid token"));
       } else {
         resolve(authObject);
